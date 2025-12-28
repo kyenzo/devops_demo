ffa4f4e770e47876712f1d8f825e34eb3d70d31a
kyenzo <kyenzo@gmail.com>
2025-12-28 12:21:25 -0700

    terraform remote state

--- terraform/envs/prod/main.tf
+++ terraform/envs/prod/main.tf
@@ -12,10 +12,10 @@ module "terraform_state_bucket" {
   }
 }
 
-# module "albs" {
-#   source = "./albs"
+# module "loadbalancer" {
+#   source = "../../modules/loadbalancer"
 
-#   lb_name         = var.lb_name
+#   name            = var.lb_name
 #   security_groups = var.security_groups
 #   subnets         = var.subnets
 #   vpc_id          = var.vpc_id
--- terraform/modules/loadbalancer/variables.tf
+++ terraform/modules/loadbalancer/variables.tf
@@ -56,47 +56,20 @@ variable "idle_timeout" {
 
 variable "target_groups" {
   description = "Map of target groups to create"
-  type = map(object({
-    name     = string
-    port     = number
-    protocol = string
-    health_check = object({
-      enabled             = optional(bool)
-      healthy_threshold   = optional(number)
-      unhealthy_threshold = optional(number)
-      timeout             = optional(number)
-      interval            = optional(number)
-      path                = optional(string)
-      matcher             = optional(string)
-      protocol            = optional(string)
-    })
-  }))
-  default = {}
+  type        = any
+  default     = {}
 }
 
 variable "listeners" {
   description = "Map of listeners to create"
-  type = map(object({
-    port            = number
-    protocol        = string
-    ssl_policy      = optional(string)
-    certificate_arn = optional(string)
-    default_action = object({
-      type             = string
-      target_group_key = optional(string)
-    })
-  }))
-  default = {}
+  type        = any
+  default     = {}
 }
 
 variable "target_group_attachments" {
   description = "Map of target group attachments"
-  type = map(object({
-    target_group_key = string
-    target_id        = string
-    port             = optional(number)
-  }))
-  default = {}
+  type        = any
+  default     = {}
 }
 
 variable "tags" {
